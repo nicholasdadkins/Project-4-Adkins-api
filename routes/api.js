b75c5bf54b1f07952597a6ae8efc3da46df8c93e
nicholasdadkins <nicholasdonadkins@gmail.com>
2021-10-15 00:02:02 -0400

    Connected mysql database

--- routes/api.js
+++ routes/api.js
@@ -115,8 +115,77 @@
 
 
 
-// module.exports = router;
+// // module.exports = router;
 
+// var express = require('express');
+// var router = express.Router();
+// let {User} = require('../lib/models')
+// /* GET home page. */
+
+// // RESTful API's'
+
+// // Identifying the actors/entities in the system
+// // Users - Noun
+// // Create - POST /users - DONE
+// // Delete - DELETE /users/1 - delete the user with id = 1
+// // Update - PUT /users/1 - update the user with id = 1
+// // Get a User - GET /users/1 - get or fetch a user with id = 1
+// // Get all users - GET /users - get or fetch all the users in the system - DONE
+
+// // Get a User - GET /users/1 - get or fetch a user with id = 1
+// router.get('/users/:id', async function(req, res, next) {
+//     let u = await User.findOne({where: {id: req.params.id}})
+//     res.json(u)
+// });
+
+
+// // Update - PUT /users/1 - update the user with id = 1
+// router.put('/users/:id', async function(req, res, next) {
+//     let u = await User.update(req.body, {where: { id: req.params.id}});
+//     res.json(u)
+// });
+
+// // Delete - DELETE /users/1 - delete the user with id = 1
+// router.delete('/users/:id', async function(req, res, next) {
+//     let u = await User.destroy({where: { id: req.params.id}});
+//     res.json(u)
+// });
+
+// // Get all users - GET /users - get or fetch all the users in the system
+// router.get('/users', async function(req, res, next) {
+//     // request came in here
+//     // this is where i write code to fetch all the users from the data base.
+//     let allUsers = await User.findAll({})
+//     // this is equivalent to this in SQL: select * from Users;
+
+
+//     // below is where i send the users i found from the DB to the front end
+//     res.json(allUsers)
+//     // res.render('index', { title: 'Express' });
+// });
+
+// // Create - POST /users
+// router.post('/users', async function(req, res, next) {
+//     // request came in here
+//     // this is where i write code to fetch all the users from the data base.
+//     let aUser = await User.create(req.body)
+//     // this is equivalent to this in SQL: select * from Users;
+
+
+//     // below is where i send the users i found from the DB to the front end
+//     res.json(aUser)
+//     // res.render('index', { title: 'Express' });
+// });
+
+
+// router.get('/', function(req, res, next) {
+//     // request came in here
+
+//     res.send('this is the home page for the API router')
+//     // res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
 var express = require('express');
 var router = express.Router();
 let {User} = require('../lib/models')
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,57 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Respond back from the users router and terminate the request');
 });
+
+
+// RESTFUL routes
+// Databases
+
+router.post('/settings/update', function(req, res, next) {
+  console.log('req.body', req.body)
+
+  res.json({success: 'ok'})
+})
+
+// <form url='http://localhost:3000/users/settings/update'> POST to http://localhost:3000/users/settings/update
+//     <input type=text name=firstName>
+//     <input type=text name=lastName>
+// </form>
+
+router.get('/settings/:name', function(req, res, next) {
+  // console.log(req)
+  console.log('req.params', req.params)
+  console.log('req.query', req.query)
+
+  // you will do some database lookups
+  // get the data from the database
+  // manipulate the data in whatever way
+  // finally send back the response
+
+  // debit the money from the user account
+  // if success create an order
+  // send an email for the order success
+  // if not enough cash, then show error
+  // maybe send an email
+  // setTimeout(() => {
+  //   res.send('Terminate the request at the john endpoint');
+    res.json({firstName: 'John', lastName: 'Doe'});
+  // }, 10000)
+
+});
+
 
 module.exports = router;
